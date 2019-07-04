ac9c02f67d4597664b54b4188049401bbe86a177
intelligent-zombie <45226010+intelligent-zombie@users.noreply.github.com>
2019-07-04 16:39:57 +0200

    change

--- Includes/optionbar.php
+++ Includes/optionbar.php
@@ -2,7 +2,7 @@
   if(!isset($_SESSION)) {
     session_start();
   }
-  
+
   echo '<div id="fileoptionbar">';
   if(isset($_SESSION["id"])) {
     echo '<a href="./index.php" class="button">Home</a>';

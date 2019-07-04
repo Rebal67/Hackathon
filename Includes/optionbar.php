8d5838e60db0b28dc56eb4525e9f8902bb0982bb
NoahKa0 <45283319+NoahKa0@users.noreply.github.com>
2019-07-04 16:35:34 +0200

    Optionbar added

---
+++ Includes/optionbar.php
@@ -0,0 +1,12 @@
+<?php
+  if(!isset($_SESSION)) {
+    session_start();
+  }
+  
+  echo '<div id="fileoptionbar">';
+  if(isset($_SESSION["id"])) {
+    echo '<a href="./index.php" class="button">Home</a>';
+    echo '<a href="#" onclick="createNewFolder()" class="button">Create new folder</a>';
+  }
+  echo '</div>';
+?>
